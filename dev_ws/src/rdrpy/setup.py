b5c8f3888ce1d31023807f38dab1fff63f79719a
bitboiii <nikita4116a@gmail.com>
2022-07-04 14:13:31 +1000

    added hsv_camera for desktop

--- dev_ws/src/rdrpy/setup.py
+++ dev_ws/src/rdrpy/setup.py
@@ -23,6 +23,7 @@ setup(
         'console_scripts': [
             'imu = rdrpy.imu:main',
             'hsv_cam = rdrpy.hsv_cam:main',
+            'hsv_cam_desktop = rdrpy.hsv_cam_desktop:main',
             'camera_processor = rdrpy.camera_processor:main',
             'camera_viewer = rdrpy.camera_viewer:main',
             'heuristic_controller = rdrpy.heuristic_controller:main',
