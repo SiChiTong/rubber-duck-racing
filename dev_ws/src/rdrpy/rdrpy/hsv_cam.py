df947ea1b588eed3e2114d61d911a035d45e2a72
bitboiii <nikita4116a@gmail.com>
2022-07-15 09:21:35 +1000

    minsize

--- dev_ws/src/rdrpy/rdrpy/hsv_cam.py
+++ dev_ws/src/rdrpy/rdrpy/hsv_cam.py
@@ -233,7 +233,7 @@ class HSVCam(Node):
             purple_mask, red_mask, green_mask = self.hsv_aux_detect(self.frame)
             params = cv2.SimpleBlobDetector_Params()
             params.filterByArea = True
-            params.minArea = 5
+            params.minArea = 100
             detector = cv2.SimpleBlobDetector_create(params)
 
             keypoints = detector.detect(green_mask)
